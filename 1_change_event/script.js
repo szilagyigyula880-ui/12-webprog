8bb3207394088bdbee0bc5cc021c263e5c1fffff
tanulo <tanulo@barossgabor.local>
2025-09-15 13:47:42 +0200

    change event JS

---
+++ 1_change_event/script.js
@@ -0,0 +1,23 @@
+const citties = document.querySelector('#citties');
+
+citties.addEventListener('change', () => {
+    const selected = citties.value;
+    console.log(selected);
+
+    const demo = document.querySelector('#demo');
+    demo.textContent = selected;
+
+    const image = document.querySelector('#image');
+    const img = document.createElement('img');
+
+    img.src = `./img/${selected}.jpg`;
+    img.alt = selected;
+    img.title = selected;
+
+    console.log(img);
+    image.append(img);
+
+    image.replaceChildren(img);
+
+
+});
