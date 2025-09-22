cbeaad32b704ec1c5621e8c1453967c21e152d70
tanulo <tanulo@barossgabor.local>
2025-09-22 14:22:40 +0200

    DOMContentLoaded event

---
+++ 2_DOMContentLoad_event/script.js
@@ -0,0 +1,13 @@
+// Városok listája
+const cities = [
+    { name: "Berlin", value: "berlin"},
+    { name: "Tokyo", value: "tokyo" },
+    { name: "Rio", value: "rio" },
+    { name: "Nairobi", value: "nairobi" },
+    { name: "Lisszabon", value: "lisbon" },
+    { name: "Moszkva", value: "moscow" },
+    { name: "Denver", value: "denver" },
+    { name: "Stockholm", value: "stockholm" },
+    { name: "Helsinki", value: "helsinki" },
+    { name: "Professzor", value: "professor" },
+];
